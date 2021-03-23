831154c52e9b38c77e9f6502fc4686ba17516a8d
Dking25 <80939087+Dking25@users.noreply.github.com>
2021-03-23 12:57:24 -0500

    Update fortune.js

--- Fortune teller/fortune.js
+++ Fortune teller/fortune.js
@@ -23,3 +23,14 @@ else if (answer === 3){
 }
 
 teller();
+
+// What is the Difference between a Front-End Developer and a Back-End Developer?
+// A Front-end Developer is the designer of the webpage, creating the functionality as well as the style of the page 
+// picking out the layout the color as well as what types of functions happen on the page 
+// the best example of this role is like being at a resturant the Front-end developer being the dining room area of the 
+// resturant as well as being the waiter 
+
+// The Back-End Devloper deals more with the data side of the website pulling information from the database that is established 
+// with the website and then pulling information from the webiste back into the data base 
+// the best example for the Back-End Developer in a resturant setting is that they are the kitchen area and provide the food and 
+// the meal to the customer and the information.
